b8870f59da4849f48af51702e337478dc9b8031d
root <root@worker1.example.com>
2022-03-20 16:43:02 +0000

    testing

---
+++ src/main/java/india_devops/App.java
@@ -0,0 +1,13 @@
+package india_devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
